97c841080d7b21c4037eb26ea6e238fce08c704b
ferenc_koler <ferenckoler@outlook.com>
2023-11-03 22:37:37 +0100

    Basic CRUD && Endpoints config

--- database/mongoose.js
+++ database/mongoose.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/taskManagerDB')
         console.log('MongoDB connected...');
     })
     .catch((err) => {
-        console.log('Error:', err);
+        console.error('Error:', err);
     });
 
 module.exports = mongoose;
